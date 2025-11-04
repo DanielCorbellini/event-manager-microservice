98c6ab3cdd4175d14f7969c93b5ebca440171a58
DanielCorbellini <daniel.corbellini28@gmail.com>
2025-11-04 20:28:06 -0300

    feat: add event controller

--- app/Http/Controllers/Events/EventsController.php
+++ app/Http/Controllers/Events/EventsController.php
@@ -2,25 +2,38 @@
 
 namespace App\Http\Controllers\Events;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\EventsService;
+use App\Http\Controllers\Controller;
+use Exception;
 
 class EventsController extends Controller
 {
+    protected $eventsService;
+
+    public function __construct(EventsService $eventsService)
+    {
+        $this->eventsService = $eventsService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
+        $events = $this->eventsService->listAll();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        if ($events->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Nenhum evento foi encontrado'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'eventos' => $events
+        ], 200);
     }
 
     /**
@@ -28,38 +41,117 @@ class EventsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                "titulo" => "required|string|max:255",
+                "data_inicio" => "required|date",
+                "data_fim" => "required|date|after_or_equal:data_inicio",
+                "local" => "required|string|max:255",
+            ]);
+
+            $event = $this->eventsService->create($validated);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Evento criado com sucesso!',
+                'data' => $event,
+            ], 201);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro de validação.',
+                'errors' => $e->errors(),
+            ], 422);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro interno ao criar evento.',
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
-    }
+        try {
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+            $event = $this->eventsService->getById($id);
+
+            return response()->json([
+                'success' => true,
+                'evento' => $event
+            ], 200);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Evento não encontrado.'
+            ], 404);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro interno ao criar evento.',
+            ], 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        //
+        try {
+            $validated = $request->validate([
+                "titulo" => "sometimes|required|string|max:255",
+                "data_inicio" => "sometimes|required|date",
+                "data_fim" => "sometimes|required|date|after_or_equal:data_inicio",
+                "local" => "sometimes|required|string|max:255",
+            ]);
+
+            $event = $this->eventsService->update($id, $validated);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Evento atualizado com sucesso!',
+                'data' => $event,
+            ], 200);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro de validação.',
+                'errors' => $e->errors(),
+            ], 422);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Evento não encontrado.'
+            ], 404);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro interno ao atualizar evento.',
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        $eventDeleted = $this->eventsService->delete($id);
+
+        if (!$eventDeleted) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Evento não encontrado.'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Evento deletado com sucesso.'
+        ], 200);
     }
 }
